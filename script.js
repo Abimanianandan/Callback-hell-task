46fc57990f7b031740ad989108453cf5410bac47
Abimanianandan <abikbm06@gmail.com>
2024-01-24 14:27:17 +0530

    callback hell

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+   document.getElementById("display").innerText="10";
+   setTimeout(()=>{
+    document.getElementById("display").innerText="9";
+    setTimeout(()=>{
+        document.getElementById("display").innerText="8";
+        setTimeout(()=>{
+            document.getElementById("display").innerText="7";
+            setTimeout(()=>{
+                document.getElementById("display").innerText="6";
+                setTimeout(()=>{
+                    document.getElementById("display").innerText="5";
+                    setTimeout(()=>{
+                        document.getElementById("display").innerText="4";
+                        setTimeout(()=>{
+                            document.getElementById("display").innerText="3";
+                            setTimeout(()=>{
+                                document.getElementById("display").innerText="2";
+                                setTimeout(()=>{
+                                    document.getElementById("display").innerText="1";
+                                    setTimeout(()=>{
+                                        document.getElementById("display").innerText="Happy Independence Day 🏳‍🌈";    
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+   },1000)
+},1000)
